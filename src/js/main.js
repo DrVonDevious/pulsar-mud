3628aed570b5efdd99bdc8e2249b1dc179e5a68d
DrVonDevious <drvondevious@gmail.com>
2020-08-01 18:52:12 -0500

    add title screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const commands = require("../js/commands.js")
 const utils = require("../js/utils.js")
+const player = require("../js/player.js")
 
+const mainDiv = document.querySelector("#main-window")
 const inputBar = document.querySelector("#input-form")
 
 // processes user input and clears the input bar
@@ -9,3 +11,21 @@ inputBar.addEventListener("submit", (event) => {
   commands.getCmd(event.target[0].value)
   event.target[0].value = ""
 })
+
+const titleScreen = (duration) => {
+  mainDiv.style.display = "none"
+  let title = document.createElement("h1")
+  title.innerText = "Pulsar"
+  title.id = "title-text"
+  document.body.appendChild(title)
+  utils.wait(() => {
+    document.body.removeChild(title)
+    mainDiv.style.display = "block"
+  }, duration)
+}
+
+const run = () => {
+  titleScreen(3000)
+}
+
+run()
